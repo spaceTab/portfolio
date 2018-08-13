7c7b3cfe15ac8716bdda330725348532e7199771
Jake P <93plaistj@protonmail.ch>
2018-08-13 16:18:10 -0400

    Polishing design - Carousel added

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,17 @@ $(() => {
         //initiating scrollSpy feature
         $('.scrollspy').scrollSpy();
         $('.parallax').parallax();
+        $('.sidenav').sidenav({
+                draggable: true
+        })
+        $('.carousel').carousel();
 
 
 
-
-
-
+        // AOS.init({
+        //         duration: 1350,
+        //         mirror: false
+        // });
 
 
         /*End of File*/
