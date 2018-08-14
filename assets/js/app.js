d4562252371d9d1d32ff685832e272fa2d8bec4a
Jake P <93plaistj@protonmail.ch>
2018-08-14 19:53:10 -0400

    Terminal Bio / typedjs added / styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,24 @@
 $(() => {
+
+       // import Typed from 'typed.js';
+
         M.AutoInit();
 
         $('.sidenav').sidenav({
                 draggable: true,
                 preventScrolling: true
-        })
+        });
+     
+        let options = {
+                strings: ["My name is Jake Plaisted. I\'m currently attending the UNH Coding Bootcamp, studying full-stack development. Trying to persue my passions for technology. I have always had a passion for computers ever since recieving my first computer, a hammy-downed  <i>Macintosh Classic 2</i> as a young kid. I have been playing with computers ever since."],
+                Speed: 150,
+                contentType: 'html',
+                showCursor: false,
+                startDelay: 2500,
+                smartBackspace: true
+        }
 
+        let typed = new Typed('.typed', options);
 
         AOS.init({
                 duration: 1350,
